93122478d88880f6d53a9344b43b0cbbb1053bfc
lesetja <lesmaribe@gmail.com>
2017-03-14 20:28:03 +0200

    added another project that implements dukesAPIs
    Signed-off-by: lesetja <lesmaribe@gmail.com>

---
+++ DukesServices/src/main/java/za/co/lesetja/dukesservices/serv/ApplicationRoot.java
@@ -0,0 +1,9 @@
+package za.co.lesetja.dukesservices.serv;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest/")
+public class ApplicationRoot extends Application {
+
+}
